f078e01cf9ba9be0be7f04386c7c0d3ae6f719e2
Abdul Mozid <mozidkhan2020@gmail.com>
2026-05-24 11:43:40 +0600

    install react-icons

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,10 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
 import Skills from "./components/Skills";
-import Projects from "./components/Projects";
+
 import EducationAndCertifications from "./components/Education";
 import ContactCTA from "./components/Contact";
+import Projects from "./project/page";
 
 export default function Home() {
   return (
---
+++ src/app/project/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectDetails = () => {
+  return <div>project detials</div>;
+};
+
+export default ProjectDetails;
